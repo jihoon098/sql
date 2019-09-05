f3faa7c841269fbbeababa6f8683bf5937d3766f
BIT <BIT@DESKTOP-1KCBVQ0>
2019-09-05 11:00:20 +0900

    join수정

--- src/select/practice3.sql
+++ src/select/practice3.sql
@@ -49,3 +49,42 @@ and c.dept_no = d.dept_no;
 
 
 -- 문제7
+select concat(first_name, ' ', last_name) as 'Engineer중 급여가 40000이상인 사원 이름'
+from employees a, titles b, salaries c
+where c.salary >= 40000
+and a.emp_no = b.emp_no
+and b.emp_no = c.emp_no
+and b.title = 'Engineer'
+and b.to_date = '9999-01-01'
+and c.to_date = '9999-01-01';
+
+
+-- 문제8
+select a.title as '직책', b.salary as '급여'
+from titles a, salaries b
+where b.salary >= 50000
+and a.emp_no = b.emp_no
+and a.to_date = '9999-01-01'
+and b.to_date = '9999-01-01'
+order by b.salary desc;
+
+
+-- 문제9
+select c.dept_name as '부서', avg(a.salary) as '연봉'
+from salaries a, dept_emp b, departments c
+where a.emp_no = b.emp_no
+and b.dept_no = c.dept_no
+and a.to_date = '9999-01-01'
+and b.to_date = '9999-01-01'
+group by c.dept_name
+order by avg(a.salary) desc;
+
+-- 문제10
+select b.title as '직책', avg(c.salary) as '연봉'
+from employees a, titles b, salaries c
+where a.emp_no = b.emp_no
+and b.emp_no = c.emp_no
+and b.to_date = '9999-01-01'
+and c.to_date = '9999-01-01'
+group by b.title
+order by avg(c.salary) desc;
